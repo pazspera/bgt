39f9c77d1635693f399b5e6f3d175191d358cf8d
Paz Spera <mspera03@gmail.com>
2023-02-26 17:58:32 -0300

    Agregué funcionalidad al btn de delete en PlayerCard. La forma de hacerlo con la options API no funciona con la composition API. Se borra en db.json pero no se actualiza la lista. Puede que sea porque tengo el componente PlayerList en el medio, capaz que haciéndolo directamente en PlayersView no tenga ese problema

--- bgt/src/components/Players/PlayerCard.vue
+++ bgt/src/components/Players/PlayerCard.vue
@@ -5,7 +5,7 @@
         <p class="m-0">{{ player.name }}</p>
         <div class="icons">
           <span class="material-icons"> edit </span>
-          <span class="material-icons"> delete </span>
+          <span @click="deletePlayer" class="material-icons"> delete </span>
         </div>
       </div>
     </div>
@@ -16,7 +16,30 @@
 export default {
   name: "PlayerCard",
   props: ["player"],
+  setup(props, context) {
+    const deletePlayer = () => {
+      let uri = `http://localhost:3000/players/${props.player.id}`;
+      fetch(uri, {
+        method: "DELETE",
+      })
+        .then(() => {
+          context.emit("deletePlayer", props.player.id);
+        })
+        .catch((err) => console.log(err));
+    };
+
+    return { deletePlayer };
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.material-icons {
+  cursor: pointer;
+  color: darkslategrey;
+
+  &:hover {
+    color: lightslategrey;
+  }
+}
+</style>
--- bgt/src/components/Players/PlayerList.vue
+++ bgt/src/components/Players/PlayerList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div v-for="player in players" :key="player.id">
-    <PlayerCard :player="player" />
+  <div v-for="player in localPlayers" :key="player.id">
+    <PlayerCard :player="player" @delete="handleDelete" />
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
 /* Components */
 import PlayerCard from "@/components/Players/PlayerCard.vue";
 
@@ -13,8 +14,23 @@ export default {
   components: { PlayerCard },
   props: ["players"],
   setup(props) {
+    // Saving the players prop on to a separate variable
+    // That way, once a player is deleted, I can filter it out of
+    // the array without getting unexpected mutation error
+    let localPlayers = reactive(props.players);
+
     console.log(props.players);
     console.log("player list");
+
+    const handleDelete = (id) => {
+      // Gets the deleted id from the PlayerCard
+      // Then we filter the players array to remove the deleted player from the list
+      localPlayers = localPlayers.value.filter((player) => {
+        return player.id !== id;
+      });
+    };
+
+    return { localPlayers, handleDelete };
   },
 };
 </script>
