020b0da33d41f9d227a9e0381451e2b20a78e1e8
Paz Spera <mspera03@gmail.com>
2023-03-06 23:32:48 -0300

    Agregué el método capitalizeString a AddPlayerView. Esta función la voy a tener que usar en AddBoardgames y AddGames. Tengo que pasar las cosas a la Composition API

--- src/views/Players/AddPlayerView.vue
+++ src/views/Players/AddPlayerView.vue
@@ -5,7 +5,7 @@
         <form @submit.prevent="handleSubmit">
           <div class="mb-3">
             <label for="name" class="form-label">Nombre</label>
-            <input type="text" v-model="newPlayerName" class="form-control" required />
+            <input type="text" v-model.trim="newPlayerName" class="form-control" required />
           </div>
           <button type="submit" class="btn btn__primary">Agregar jugador</button>
         </form>
@@ -22,17 +22,19 @@ export default {
       newPlayerName: "",
     };
   },
-  mounted(){
+  mounted() {
     document.title = "Agregar Jugador - Board Game Tracker";
   },
   methods: {
     handleSubmit() {
+      let capitalizedName = this.capitalizeString(this.newPlayerName);
+
       let player = {
-        name: this.newPlayerName,
+        name: capitalizedName,
       };
       console.log(player);
 
-      fetch("http://localhost:3000/players", {
+      /* fetch("http://localhost:3000/players", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(player),
@@ -42,7 +44,21 @@ export default {
         })
         .catch((err) => {
           console.log(err);
-        });
+        }); */
+    },
+    capitalizeString(string) {
+      let arrayOfWords = string.split(/[\s,\t,\n]+/);
+      let capitalizedString;
+
+      let capitalizeWord = (string) => {
+        let firstLetter = string[0].toUpperCase();
+        let rest = string.slice(1).toLowerCase();
+        return firstLetter + rest;
+      };
+
+      capitalizedString = arrayOfWords.map(capitalizeWord).join(" ");
+
+      return capitalizedString;
     },
   },
 };
