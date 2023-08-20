d382b17de90efd88e7254bfe6d182abca77ee7b7
Paz Spera <mspera03@gmail.com>
2023-08-20 11:29:43 -0300

    Moví Bottom Nav a su propio componente. Modificaciones de estilo del componente

--- src/App.vue
+++ src/App.vue
@@ -14,38 +14,19 @@
       <i class="fa-solid fa-magnifying-glass"></i>
       <v-icon icon="mdi:mdi-minus" />
     </v-main>
-    <v-bottom-navigation class="bg-neutral-800" style="height: 72px">
-      <router-link class="nav-link" :to="{ name: 'BoardGames' }">
-        <v-btn value="ludoteca">
-          <i class="fa-solid fa-dice"></i>
-          <span>Ludoteca</span>
-        </v-btn>
-      </router-link>
-
-      <router-link class="nav-link" :to="{ name: 'Players' }">
-        <v-btn value="jugadores">
-          <i class="fa-solid fa-users"></i>
-          <span>Jugadores</span>
-        </v-btn>
-      </router-link>
-
-      <router-link class="nav-link" :to="{ name: 'Games' }">
-        <v-btn value="partidas">
-          <i class="fa-solid fa-box-archive"></i>
-          <span>Partidas</span>
-        </v-btn>
-      </router-link>
-    </v-bottom-navigation>
+    <BottomNavigation />
   </v-app>
 </template>
 
 <script>
+import BottomNavigation from "./components/BottomNavigation.vue";
 /* import NavBar from "@/components/NavBar.vue"; */
 
 export default {
   name: "App",
   components: {
     /* NavBar */
+    BottomNavigation,
   },
 };
 </script>
---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-bottom-navigation class="bg-neutral-800 bottom-nav">
+    <router-link class="nav-link" :to="{ name: 'BoardGames' }">
+      <v-btn value="ludoteca">
+        <i class="fa-solid fa-dice bottom-nav__icon"></i>
+        <span class="mb-2">Ludoteca</span>
+      </v-btn>
+    </router-link>
+
+    <router-link class="nav-link" :to="{ name: 'Players' }">
+      <v-btn value="jugadores">
+        <i class="fa-solid fa-users bottom-nav__icon"></i>
+        <span class="mb-2">Jugadores</span>
+      </v-btn>
+    </router-link>
+
+    <router-link class="nav-link" :to="{ name: 'Games' }">
+      <v-btn value="partidas">
+        <i class="fa-solid fa-box-archive bottom-nav__icon"></i>
+        <span class="mb-2">Partidas</span>
+      </v-btn>
+    </router-link>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "BottomNavigation",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
+
+.bottom-nav {
+  height: 80px;
+  padding-bottom: $size8;
+}
+
+.bottom-nav__icon {
+  font-size: $size5;
+  margin-bottom: $size10;
+}
+
+.nav-link {
+  margin-bottom: $size9;
+}
+</style>
