0162d6d98e476da2fccd9643850e147cd6e3a4d0
Paz Spera <mspera03@gmail.com>
2023-09-29 00:03:55 -0300

    Agregué input de Juego en AddGame. En vez de mostrar el nombre del juego en el input, se podría poner como subtítulo. No está tomando el v-model para mostrarlo en el input. Y no se va a poder modificar, así que no hace falta que esté en el input

--- src/views/Games/AddGame.vue
+++ src/views/Games/AddGame.vue
@@ -1,71 +1,83 @@
 <template>
-  <div>
-    <h1>The adding of the game</h1>
-  </div>
+  <HeroSection :title="heroTitle" :bg-class="bgClass" />
+  <v-container class="my-5">
+    <v-row >
+      <v-col cols="12" md="8" offset-md="2">
+        <v-text-field label="Juego" model-value="boardGameName"></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { computed, onBeforeMount, onMounted /* ref */ } from "vue";
+import { computed, onBeforeMount, onMounted, reactive, ref } from "vue";
 import { useBoardGameStore } from "@/stores/BoardGameStore";
 import { useGameStore } from "@/stores/GameStore";
+import HeroSection from "@/components/HeroSection.vue";
 
 export default {
   name: "AddGame",
+  components: { HeroSection },
+  data() {
+    return {
+      heroTitle: "Agregar nueva partida",
+      bgClass: "gamesBg",
+    };
+  },
   mounted() {
     document.title = "Agregar Partida - Board Game Tracker";
   },
   setup() {
     const boardGameStore = useBoardGameStore();
-    /* const boardGameId = ref(null); */
     const gameStore = useGameStore();
     // The computed here keeps a sync copy of the value
     // of selectedBoardGameId
     const selectedBoardGameId = computed(() => gameStore.selectedBoardGameId);
     // This object is going to hold the boardgame
-    /* const boardGame = ref(null); */
-    /* const testBoardGame = ref(null); */
-    /* const fetchedBoardGame = ref(null); */
-    let otraVariable = null;
+    const boardGame = ref(null);
+    // This variable is gonna be false by default. Once
+    // boardGame.value exists, it changes to true to show
+    // the form
+    const showForm = ref(false);
+
+    // Form to add the new game
+    const newGameForm = reactive({
+      startDate: "",
+      boardGameId: "",
+      playerIdsList: [],
+      winnerPlayerId: "",
+      description: "",
+    });
+
+    // Saving the boardgame name on a variable
+    // to use v-model and show it on the Juego input
+    const boardGameName = ref(null);
 
     onBeforeMount(async () => {
       console.log("onBeforeMount");
       console.log("Selected boardgame ID: " + selectedBoardGameId.value);
 
-      /* try {
+      try {
         boardGame.value = await boardGameStore.getBoardgame(selectedBoardGameId.value);
-        console.log("The boardgame is: " + boardGame.value);
-        console.log("Tipo de boardGame.value: " + typeof boardGame.value);
 
         if (boardGame.value) {
           console.log("BoardGame: " + boardGame.value.name);
+          console.log(showForm.value);
+          showForm.value = true;
+          console.log(showForm.value);
         }
       } catch (err) {
         console.err("Error fetching boardgame:" + err);
-      } */
-
-      // trying to fetch a fixed id 5
-      /* try {
-        console.log("Before fetch, testBoardGame.value ", testBoardGame.value);
-        testBoardGame.value = await boardGameStore.getBoardgame(5);
-        console.log("After fetch, testBoardGame.value ", testBoardGame.value);
-
-        if (testBoardGame.value) {
-          console.log("testBoardGame: " + testBoardGame.value.name);
-        }
-      } catch (err) {
-        console.err("Error fetching boardgame:" + err);
-      } */
-
-      // still trying to fetch fixed id 5
-      try {
-        // fetchedBoardGame.value = await boardGameStore.getBoardgame(5);
-        otraVariable = await boardGameStore.getBoardgame(5);
-        debugger;
-        console.log("otravariable", otraVariable);
-        /* console.log("fetched board game: ", fetchedBoardGame.value); */
-      } catch (error) {
-        console.log("error fetching boardgame: ", error);
       }
+
+      // Asign the value of the boardGameId that's
+      // coming from the boardGameCard to
+      // newGameForm once it's been fetched
+      newGameForm.boardGameId = selectedBoardGameId.value;
+      console.log("newGameForm.boardGameId: ", newGameForm.boardGameId);
+
+      // Asign boardgame name to show it on input
+      boardGameName.value = boardGame.value.name;
     });
 
     onMounted(async () => {
@@ -73,7 +85,7 @@ export default {
       console.log("Selected boardgame ID: " + selectedBoardGameId.value);
     });
 
-    return {};
+    return { newGameForm };
   },
 };
 </script>
